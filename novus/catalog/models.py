c3451abfdf0313ac769241b88d5c5fd69c82b72e
coIorbIind <labinfpin13@gmail.com>
2021-11-27 00:54:25 +0300

    first day. Added all models(first version)

--- novus/catalog/models.py
+++ novus/catalog/models.py
@@ -1,26 +1,62 @@
-# from datetime import timedelta, datetime
-# from django.db import models
-#
-#
-# class Product(models.Model):
-#     name = models.CharField(max_length=20)
-#     price = models.IntegerField(default=10)
-#     time = models.IntegerField(default=10)
-#     service = models.CharField(max_length=20)
-#
-#     def __str__(self) -> str:
-#         return self.name
-
 from django.db import models
 
 
-class Author(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
+class Service(models.Model):
+    service_name = models.CharField(max_length=25)
+    delivery_price = models.FloatField()
+
+    def __str__(self):
+        return self.service_name
 
 
-class Article(models.Model):
-    title = models.CharField(max_length=120)
+class Category(models.Model):
+    category_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Customer(models.Model):
+    login_email = models.CharField(max_length=25)
+    password = models.CharField(max_length=20)
+    user_token = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.login_email
+
+
+class Good(models.Model):
+    name = models.CharField(max_length=25)
     description = models.TextField()
-    body = models.TextField()
-    author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
+    img_id = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class GoodService(models.Model):
+    good = models.ForeignKey(Good, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    price = models.FloatField()
+    time = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.id}. {self.good} в сервисе {self.service}'
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.id}. {self.customer}'
+
+
+class BuyInfo(models.Model):
+    gs = models.ForeignKey(GoodService, on_delete=models.CASCADE)
+    count = models.IntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+
+
