062b11f8731be3d6bde0f5a325569bed04b0430d
ALeX <ometov.sasha@mail.ru>
2021-11-27 05:02:14 +0300

    create mainpage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,26 +4,24 @@ import { Link } from "react-router-dom";
 import storesContext from '@/contexts/store';
 import { routesMap } from '@/router';
 
+import Header from '@/components/header/Header.js';
+import CategoryMenu from '@/components/categoryMenu/CategoryMenu.js';
+import Store from '@/components/store/Store.js';
+
 function MainPage(){
 	// let { products } = useContext(storesContext); // подключение контекста
 	// let { ... } = cart; // Распаковка методов
 	
 	return <>
-		<Header>
-			<Logo></Logo>
-			<Search></Search>
-			<Login></Login>
-			<Cart></Cart>
-		</Header>
 
-		<CategoryMenu></CategoryMenu>
-
-		<Store></Store>
-		
-
-		<h1>Index</h1>
-		<Link to={routesMap.root} className="">Next step</Link>
+		<Header></Header>
+		<main style={{ display: 'flex' }}>
+			<CategoryMenu></CategoryMenu>
+			<Store></Store>
+		</main>
 	</>
 }
+
+
 
 export default observer(MainPage);
