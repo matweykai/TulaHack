5113b4425eba5a4521d9769312ab11c7db000d8d
coIorbIind <91391687+coIorbIind@users.noreply.github.com>
2021-11-27 09:58:49 +0300

    Kostya (#1)
    * first day. Added all models(first version)
    
    * modified .gitignore
    
    * ignored .idea

--- novus/catalog/admin.py
+++ novus/catalog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from .models import Category, Good, Service, GoodService, BuyInfo, Order, Customer
 
-from .models import Article, Author
-admin.site.register(Article)
-admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Service)
+admin.site.register(Good)
+admin.site.register(Customer)
+admin.site.register(GoodService)
+
